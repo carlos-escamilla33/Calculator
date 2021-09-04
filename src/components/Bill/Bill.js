8b72f3cf65a61e3713c70e04f7db391a868dfeb2
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-09-03 23:37:20 -0700

    adding styling

--- src/components/Bill/Bill.js
+++ src/components/Bill/Bill.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
+import "./Bill.css"
 import Tip from "../Tip/Tip.js"
 import PeopleTotal from "../PeopleTotal/PeopleTotal.js"
 
 const Bill = () => {
     return (
-        <form>
-            <p>Bill</p>
+        <form className="container">
+            <p className="title">Bill</p>
             <input
+            className="billInput"
             placeholder="$"
              />
              <p>Select Tip %</p>
